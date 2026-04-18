b874cd7c19f167e272d0e2027e9ce83083f39089
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-04-18 13:06:54 +0530

    [Abhishek Sharma] Refactor. Fixed the code for fix the login on production

--- QuantityMeasurementApplication/src/main/java/com/apps/quantitymeasurement/controller/QuantityMeasurementController.java
+++ QuantityMeasurementApplication/src/main/java/com/apps/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/quantity")
-@CrossOrigin(origins = {"http://localhost:5175/", "http://127.0.0.1:5500"})
+@CrossOrigin(origins = {"http://localhost:5173/", "http://127.0.0.1:5500"})
 public class QuantityMeasurementController {
 
     private final IQuantityMeasurementService service;
